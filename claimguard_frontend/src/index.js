744ce3626bb94d5c664c895b8d1dd8430b0e8061
VAMSI <VAMSI@MacBook-Air-5.local>
2026-05-05 21:35:58 -0400

    fixing

--- claimguard_frontend/src/index.js
+++ claimguard_frontend/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import {
-  ClerkProvider
-} from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY =
   "pk_test_aHVtYW5lLWNyYXlmaXNoLTQ3LmNsZXJrLmFjY291bnRzLmRldiQ";
